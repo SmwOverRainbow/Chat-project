a28365cb18c5a9c2a0d2293b27a7f8171de2f69c
mshcherbina <marinashcherbina89@gmail.com>
2023-10-23 02:06:55 +0300

    Add wrappers for paths and dictionaries

--- frontend/src/utils/helpers.js
+++ frontend/src/utils/helpers.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 import * as filter from 'leo-profanity';
 
-filter.loadDictionary('ru');
-filter.loadDictionary('en');
+const apiPath = '/api/v1';
+export const pathTo = {
+  login: () => [apiPath, 'login'].join('/'),
+  signup: () => [apiPath, 'signup'].join('/'),
+  getData: () => [apiPath, 'data'].join('/'),
+};
+
+export const loadDictionaries = () => {
+  filter.loadDictionary('ru');
+  filter.loadDictionary('en');
+};
 
 export const getData = async (token) => {
-  const response = await axios.get('/api/v1/data', { headers: { Authorization: `Bearer ${token}` } });
+  const response = await axios.get(pathTo.getData(), { headers: { Authorization: `Bearer ${token}` } });
   return response.data;
 };
 
